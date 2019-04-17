367f810b5d808257c9952d8ff4f13bfc7db9893d
Andrew Po <ensimgen@gmail.com>
2019-04-16 20:24:09 -0700

    Update aff_z.c

--- exam01/aff_z.c
+++ exam01/aff_z.c
@@ -12,9 +12,25 @@
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int		main(int argc, char *argv[])
 {
-	write(1, "z", 1);
-	write(1, "\n", 1);
+	int		i;
+
+	i = 0;
+	if (argc != 2)
+		write(1, "z\n", 2);
+	else
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] == 'z')
+			{
+				write(1, "z", 1);
+				break ;
+			}
+			i += 1;
+		}
+		write(1, "\n", 1);
+	}
 	return (0);
 }

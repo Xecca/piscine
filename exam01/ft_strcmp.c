7b554863b351d2afb64f1a6b950db776896a125d
Andrew Po <ensimgen@gmail.com>
2019-04-18 22:52:46 -0700

    Update ft_strcmp.c

--- exam01/ft_strcmp.c
+++ exam01/ft_strcmp.c
@@ -12,10 +12,10 @@
 
 int ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
-	{
-		s1 += 1;
-		s2 += 2;
-	}
-	return (*s1 - *s2);
+	int i;
+	
+	i = 0;
+	while (s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
